1d14f4b8a27d71adae302fa71f0677e0cbfedef2
Hyosung96 <70612891+Hyosung96@users.noreply.github.com>
2023-08-17 00:47:11 +0900

    [Silver V] Title: 다리 놓기, Time: 208 ms, Memory: 19744 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // 입력이랑 결과 보니..
+        // 순열과 조합
+        // mCn
+        // m!/n!(m-n)!
+        // 팩토리얼 재귀 구현
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++){
+            st = new StringTokenizer(br.readLine());
+            BigInteger N = new BigInteger(st.nextToken());
+            BigInteger M = new BigInteger(st.nextToken());
+            BigInteger sub = M.subtract(N);
+            N = recursion(N);
+            M = recursion(M);
+            sub = recursion(sub);
+            System.out.println(M.divide(N.multiply(sub)));
+        }
+    }
+    public static BigInteger recursion(BigInteger num){
+        if(num.compareTo(BigInteger.ONE) < 1) return BigInteger.ONE;
+        return num.multiply(recursion(num.subtract(BigInteger.ONE)));
+    }
+}
