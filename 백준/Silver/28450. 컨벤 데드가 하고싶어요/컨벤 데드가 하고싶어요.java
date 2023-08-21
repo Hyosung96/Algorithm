e16b20a3c697b355b6e447733722f4edd3dfb483
Hyosung96 <70612891+Hyosung96@users.noreply.github.com>
2023-08-21 17:54:44 +0900

    Update 컨벤 데드가 하고싶어요.java

--- 백준/Silver/28450. 컨벤 데드가 하고싶어요/컨벤 데드가 하고싶어요.java
+++ 백준/Silver/28450. 컨벤 데드가 하고싶어요/컨벤 데드가 하고싶어요.java
@@ -6,9 +6,6 @@ public class Main {
     private static long[][] dp;
 
     public static void main(String[] args) throws IOException {
-        // DP 바텀업
-        // 행, 열의 마지막 라인일 경우 분기
-        // 아닐 경우 밑, 오른쪽 인덱스 중 값이 작은 것 += 연산
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -25,7 +22,6 @@ public class Main {
         int H = Integer.parseInt(br.readLine());
 
         for (int i = N - 1; i >= 0; i--) {
-            //바텀업 메모이제이션, 점화식은..
             //DP(i,j) = 도착지 부터 0,0 인덱스까지의 최적의 해
             //점화식: DP(i,j) += min(DP(i+1, j), DP(i, j+1))
             for (int j = M - 1; j >= 0; j--) {
